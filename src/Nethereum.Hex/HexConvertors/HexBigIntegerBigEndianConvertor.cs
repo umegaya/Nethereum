62a6578c72aa942c2da5c965512b283352613b94
Takehiro Iyatomi <iyatomi@gmail.com>
2018-07-23 13:27:29 +0900

    workaround for Unity calls wrong interface for BigInteger.ToHex extension

--- src/Nethereum.Hex/HexConvertors/HexBigIntegerBigEndianConvertor.cs
+++ src/Nethereum.Hex/HexConvertors/HexBigIntegerBigEndianConvertor.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Numerics;
+using System.Linq;
 using Nethereum.Hex.HexConvertors.Extensions;
 
 namespace Nethereum.Hex.HexConvertors
@@ -7,7 +9,16 @@ namespace Nethereum.Hex.HexConvertors
     {
         public string ConvertToHex(BigInteger newValue)
         {
-            return newValue.ToHex(false);
+            if (newValue.Sign < 0) throw new Exception("Hex Encoding of Negative BigInteger value is not supported");
+            if (newValue == 0) return "0x0";
+            byte[] bytes;
+            if (BitConverter.IsLittleEndian != false)
+                bytes = newValue.ToByteArray().Reverse().ToArray();
+            else
+                bytes = newValue.ToByteArray().ToArray();
+
+            return "0x" + bytes.ToHexCompact();
+            //return newValue.ToHex(false);
         }
 
         public BigInteger ConvertFromHex(string hex)
